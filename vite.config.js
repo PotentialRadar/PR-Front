87aeab86dcf19896e8a1f61649a05355a62299a8
HEEJIN LIM <xeexin0331@gmail.com>
2025-08-20 11:45:27 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  server: {
-    port: process.env.VITE_FRONT_PORT || 5173,
-    proxy: {
-      "/api": {
-        target: `http://localhost:${process.env.VITE_BACK_PORT || 8080}`,
-        changeOrigin: true,
-        secure: false,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
+    server: {
+      port: parseInt(env.VITE_FRONT_PORT) || 5173,
+      proxy: {
+        "/api": {
+          target: `http://localhost:${env.VITE_BACK_PORT || 8080}`,
+          changeOrigin: true,
+          secure: false,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
         },
       },
     },
-  },
+  };
 });
