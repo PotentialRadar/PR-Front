f5d73174f34fce4ecadbbca3d8663a086450dbaf
HEEJIN LIM <xeexin0331@gmail.com>
2025-08-20 11:34:37 +0900

    feat:make .env
    -ENV 파일 만들고, 변수처리

--- src/constants/oauth.js
+++ src/constants/oauth.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || `http://localhost:${import.meta.env.VITE_BACK_PORT || 8080}`;
 
 export const KAKAO_AUTH_URL = `${BASE_URL}/oauth2/authorization/kakao`;
 export const GOOGLE_AUTH_URL = `${BASE_URL}/oauth2/authorization/google`;
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   server: {
+    port: process.env.VITE_FRONT_PORT || 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8082',
+      "/api": {
+        target: `http://localhost:${process.env.VITE_BACK_PORT || 8080}`,
         changeOrigin: true,
         secure: false,
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
       },
     },
   },
