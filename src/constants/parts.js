20b4d05041ff78c14081c6111fc03d2f6be74884
hwi <awdqse6312@naver.com>
2025-08-24 15:45:02 +0900

    feat/마이페이지 좋아요(프로젝트)

--- src/constants/parts.js
+++ src/constants/parts.js
@@ -1,21 +1,16 @@
 export const PART_OPTIONS = [
-    { value: 'Frontend',  label: '프론트엔드' },
-    { value: 'Backend',   label: '백엔드' },
-    { value: 'Fullstack', label: '풀스택' },
-    { value: 'Mobile',   label: '모바일' },
-    { value: 'DevOps',   label: '데브옵스' },
-    { value: 'Data Engineering', label: '데이터 엔지니어링' },
+    { value: '프론트엔드',  label: '프론트엔드' },
+    { value: '백엔드',   label: '백엔드' },
+    { value: '풀스택', label: '풀스택' },
+    { value: '모바일',   label: '모바일' },
+    { value: '데브옵스',   label: '데브옵스' },
+    { value: '데이터사이언스', label: '데이터사이언스' },
     { value: 'AI/ML',    label: 'AI/ML' },
-    { value: 'Database', label: '데이터베이스' },
-    { value: 'Cloud',    label: '클라우드' },
-    { value: 'Testing/CI', label: '테스팅/CI' },
-    { value: 'Game',     label: '게임' },
-    { value: 'Embedded', label: '임베디드' },
-    { value: 'Security', label: '보안' },
-    { value: 'Blockchain', label: '블록체인' },
-    { value: 'QA',       label: 'QA' },
-    { value: 'PM',       label: 'PM' },
-    { value: 'UI/UX',    label: 'UI/UX' }
+    { value: '게임개발',     label: '게임' },
+    { value: '보안',     label: '보안' },
+    { value: 'QA/테스터', label: 'QA/테스터' },
+    { value: 'UI/UX디자인', label: 'UI/UX디자인' },
+    { value: 'PM/기획',   label: 'PM/기획' }
 ];
 
 export const EXPERIENCE_RANGE_OPTIONS = [
